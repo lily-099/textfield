a463f9c167e58a514e7c67d02ee6da35dd50011d
Parabeac-Nest <admin@parabeac.com>
2022-05-30 21:51:45 +0000

    Auto-generated commit by Parabeac Nest

--- lib/screens/page_1/i_phone13mini_1.g.dart
+++ lib/screens/page_1/i_phone13mini_1.g.dart
@@ -5,9 +5,7 @@
 // *********************************************************************************
 
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:auto_size_text/auto_size_text.dart';
-import 'package:textfield/controller/tag/follow_rectangle_custom.dart';
 
 class IPhone13mini1 extends StatefulWidget {
   const IPhone13mini1({
@@ -25,146 +23,6 @@ class _IPhone13mini1 extends State<IPhone13mini1> {
     return Material(
       color: Colors.white,
       child: Stack(children: [
-        Positioned(
-          left: 0,
-          width: 375.0,
-          top: 0,
-          height: 44.0,
-          child: Container(
-              width: 375.000,
-              height: 44.000,
-              decoration: BoxDecoration(),
-              child: Stack(children: [
-                Positioned(
-                  right: 14.0,
-                  width: 68.0,
-                  top: 15.0,
-                  height: 16.0,
-                  child: Container(
-                      width: 68.000,
-                      height: 16.000,
-                      decoration: BoxDecoration(),
-                      child: Stack(children: [
-                        Positioned(
-                          right: 51.0,
-                          width: 17.0,
-                          top: 2.0,
-                          height: 10.667,
-                          child: SvgPicture.asset(
-                            'assets/images/cellularconnection.svg',
-                            package: 'textfield',
-                            width: 17.000,
-                            height: 10.667,
-                            fit: BoxFit.none,
-                          ),
-                        ),
-                        Positioned(
-                          left: 20.0,
-                          width: 15.333,
-                          top: 1.0,
-                          height: 11.0,
-                          child: SvgPicture.asset(
-                            'assets/images/wifi.svg',
-                            package: 'textfield',
-                            width: 15.333,
-                            height: 11.000,
-                            fit: BoxFit.none,
-                          ),
-                        ),
-                        Positioned(
-                          right: 0,
-                          width: 25.0,
-                          top: 2.0,
-                          height: 12.0,
-                          child: Container(
-                              width: 25.000,
-                              height: 12.000,
-                              decoration: BoxDecoration(),
-                              child: Stack(children: [
-                                Positioned(
-                                  left: 0,
-                                  width: 22.0,
-                                  top: 0.333,
-                                  height: 11.333,
-                                  child: Container(
-                                    width: 22.000,
-                                    height: 11.333,
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(2.6666667461395264)),
-                                      border: Border.all(
-                                        color: Color(0xff000000),
-                                        width: 1,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 23.0,
-                                  width: 1.333,
-                                  top: 4.0,
-                                  height: 4.0,
-                                  child: SvgPicture.asset(
-                                    'assets/images/cap.svg',
-                                    package: 'textfield',
-                                    width: 1.333,
-                                    height: 4.000,
-                                    fit: BoxFit.none,
-                                  ),
-                                ),
-                                Positioned(
-                                  left: 2.0,
-                                  width: 18.0,
-                                  top: 2.333,
-                                  height: 7.333,
-                                  child: Container(
-                                    width: 18.000,
-                                    height: 7.333,
-                                    decoration: BoxDecoration(
-                                      color: Colors.black,
-                                      borderRadius: BorderRadius.all(
-                                          Radius.circular(1.3333333730697632)),
-                                    ),
-                                  ),
-                                ),
-                              ])),
-                        ),
-                      ])),
-                ),
-                Positioned(
-                  left: 21.0,
-                  width: 56.0,
-                  top: 8.0,
-                  height: 23.0,
-                  child: Container(
-                      width: 56.000,
-                      height: 23.000,
-                      decoration: BoxDecoration(),
-                      child: Stack(children: [
-                        Positioned(
-                          left: 0.333,
-                          right: 1.667,
-                          top: 6.0,
-                          height: 18.0,
-                          child: Container(
-                              width: 54.000,
-                              height: 18.000,
-                              child: AutoSizeText(
-                                '9:27',
-                                style: TextStyle(
-                                  fontFamily: 'SF Pro Text',
-                                  fontSize: 15,
-                                  fontWeight: FontWeight.w600,
-                                  letterSpacing: -0.3333333432674408,
-                                  color: Colors.black,
-                                ),
-                                textAlign: TextAlign.center,
-                              )),
-                        ),
-                      ])),
-                ),
-              ])),
-        ),
         Positioned(
           left: 16.0,
           width: 343.0,
@@ -189,8 +47,7 @@ class _IPhone13mini1 extends State<IPhone13mini1> {
                   width: 343.0,
                   top: 0,
                   height: 52.0,
-                  child: FollowRectangleCustom(
-                      child: Container(
+                  child: Container(
                     width: 343.000,
                     height: 52.000,
                     decoration: BoxDecoration(
@@ -201,7 +58,7 @@ class _IPhone13mini1 extends State<IPhone13mini1> {
                         width: 2,
                       ),
                     ),
-                  )),
+                  ),
                 ),
                 Positioned(
                   left: 132.287,
@@ -226,17 +83,34 @@ class _IPhone13mini1 extends State<IPhone13mini1> {
               ])),
         ),
         Positioned(
-          left: 116.0,
-          width: 144.0,
-          top: 102.0,
-          height: 144.0,
-          child: Image.asset(
-            'assets/images/ellipse1.png',
-            package: 'textfield',
-            width: 144.000,
-            height: 144.000,
-            fit: BoxFit.none,
-          ),
+          left: 106.0,
+          width: 164.0,
+          top: 92.0,
+          height: 164.0,
+          child: Container(
+              padding: EdgeInsets.only(
+                left: 10,
+                right: 10,
+                top: 10,
+                bottom: 10,
+              ),
+              decoration: BoxDecoration(),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                        height: 144.0,
+                        width: 144.0,
+                        child: Image.asset(
+                          'assets/images/ellipse1.png',
+                          package: 'textfield',
+                          width: 144.000,
+                          height: 144.000,
+                          fit: BoxFit.none,
+                        )),
+                  ])),
         ),
         Positioned(
           left: 37.0,
